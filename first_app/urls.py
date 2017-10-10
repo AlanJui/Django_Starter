a08a8556a5474babb26ba02632f83fad0ca1c259
Alan Jui <alanjui.1960@gmail.com>
2017-10-10 16:41:56 +0800

    引用 Relative URLss

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
 from first_app import views
 
+# Template Tagging
+app_name = 'first_app'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^users$', views.users, name='users' ),
     url(r'^users/new$', views.new_user, name='New User' ),
     url(r'^users/list$', views.users_list, name='users_list'),
     url(r'^my_form$', views.my_form, name='my_form'),
-    url(r'^contact$', views.contact, name='contact')
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^relative$', views.relative, name='relative'),
+    url(r'^others$', views.others, name='others')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -16,6 +16,12 @@ def index(request):
     }
     return render(request, 'first_app/index.html', context=date_dict)
 
+def others(request):
+    return render(request, 'first_app/others.html')
+
+def relative(request):
+    return render(request, 'first_app/relative_url_template.html')
+
 def new_user(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
